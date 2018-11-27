71440fe3bfd27019228121b6025851fb93c0ec7d
Ilmari <ilmari.koskinen1@gmail.com>
2018-11-27 16:38:04 +0200

    add start and stop controllers for the ticker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Drums from './modules/drums';
 
 class App extends Component {
+  ticker = null;
   state = {
     sequence: 0,
     sequenceCount: 8,
@@ -9,19 +10,39 @@ class App extends Component {
 
   componentDidMount() {
     const { ticker } = this.props;
+    this.ticker = ticker;
 
     ticker.onTick = () => {
       this.setState(({ sequence, sequenceCount }) => ({
         sequence: (sequence + 1) % sequenceCount,
       }));
     };
-
-    ticker.start();
   }
+
+  startSequence = () => {
+    this.setState(
+      () => ({
+        sequence: -1,
+      }),
+      () => {
+        this.ticker.start();
+      }
+    );
+  };
+
+  stopSequence = () => {
+    this.ticker.stop();
+  };
 
   render() {
     const { sequence } = this.state;
-    return <Drums sequence={sequence} />;
+    return (
+      <div>
+        <button onMouseDown={this.startSequence}>Start</button>
+        <button onMouseDown={this.stopSequence}>Stop</button>
+        <Drums sequence={sequence} />
+      </div>
+    );
   }
 }
 
--- src/modules/drums/index.js
+++ src/modules/drums/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Drums extends Component {
+  getSnapshotBeforeUpdate(prevProps) {
+    const nextSequenceStepReceived =
+      prevProps.sequence !== this.props.sequence && this.props.sequence >= 0;
+    if (nextSequenceStepReceived) {
+      // play note at sequence this.props.sequence
+    }
+    return null;
+  }
+
+  componentDidUpdate() {}
+
   render() {
     return <div>Drums, {this.props.sequence}</div>;
   }
